55e0a7f3d20f464eb82d12ee6e81b4470f6e6488
reactorsalman <rajsalman3333@gmail.com>
2022-05-23 18:02:19 +0530

    css fixes

--- src/components/searchResult.component.tsx
+++ src/components/searchResult.component.tsx
@@ -7,7 +7,6 @@ import { checkUnCheck, filterBy } from "../utils";
 
 const SearchResult = (props: BookingResponse) => {
 	const holidayData = props.holidays;
-
 	const [selectedStars, setSelectedStars] = useState<string[]>([]);
 	const [selectedPricePerPerson, setSelectedPricePerPerson] = useState<string[]>([]);
 	const [selectedFacilities, setSelectedFacilities] = useState<string[]>([]);
@@ -25,44 +24,44 @@ const SearchResult = (props: BookingResponse) => {
 		<div className={styles["grid"]}>
 			<div className={styles["col"]}>
 				<div className={styles["filter-container"]}>
-					<h2>Filter by:</h2>
+					<label className={styles["label-heading"]}>Filter by:</label>
 					<div className={styles["filter-label-margin"]}>
-						<h3>Star rating</h3>
+						<label className={styles["label-subheading"]}>Star rating</label>
 						{ratings.map((option) => (
 							<label className={styles["checkbox-container"]} key={`star${option.value}`}>{option.label}
 								<input
 									type="checkbox"
 									id={option.value}
 									checked={selectedStars.includes(option.value)}
-									onChange={(e) => { setSelectedStars((prev: string[]) => checkUnCheck(prev, e.target.id)) }}
+									onChange={(e) => { setSelectedStars((prev: string[]) => checkUnCheck(prev, (e.target as HTMLInputElement).id)) }}
 								/>
 								<span className={styles["checkmark"]}></span>
 							</label>
 						))}
 					</div>
 					<div className={styles["filter-label-margin"]}>
-						<h3>Price per person</h3>
+						<label className={styles["label-subheading"]}>Price per person</label>
 						{pricePerPerson.map((option) => (
 							<label className={styles["checkbox-container"]} key={`price${option.value}`}>{option.label}
 								<input
 									type="checkbox"
 									id={`${option.min}-${option.max}`}
 									checked={selectedPricePerPerson.includes(`${option.min}-${option.max}`)}
-									onChange={(e) => { setSelectedPricePerPerson((prev: string[]) => checkUnCheck(prev, e.target.id)) }}
+									onChange={(e) => { setSelectedPricePerPerson((prev: string[]) => checkUnCheck(prev, (e.target as HTMLInputElement).id)) }}
 								/>
 								<span className={styles["checkmark"]}></span>
 							</label>
 						))}
 					</div>
 					<div className={styles["filter-label-margin"]}>
-						<h3>Hotel facilities</h3>
+						<label className={styles["label-subheading"]}>Hotel facilities</label>
 						{facilities.map((option) => (
 							<label className={styles["checkbox-container"]} key={`facility${option.value}`}>{option.label}
 								<input
 									type="checkbox"
 									id={option.label}
 									checked={selectedFacilities.includes(option.label)}
-									onChange={(e) => { setSelectedFacilities((prev: string[]) => checkUnCheck(prev, e.target.id)) }}
+									onChange={(e) => { setSelectedFacilities((prev: string[]) => checkUnCheck(prev, (e.target as HTMLInputElement).id)) }}
 								/>
 								<span className={styles["checkmark"]}></span>
 							</label>
@@ -79,26 +78,27 @@ const SearchResult = (props: BookingResponse) => {
 									alt="hotel" width="350" height="350" className="rounded" />
 							</div>
 							<div className={styles["col"]}>
-								<h3 className="">{hotelData?.hotel?.name}</h3>
-								<h3>${hotelData?.pricePerPerson}</h3>
-								<div className="card border-0">
-									{hotelData?.hotel?.content?.hotelDescription.slice(0, 350) + "..."}
+								<label className={styles["label-heading"]}>{hotelData?.hotel?.name}</label>
+								<label className={styles["label-heading"]}>€{hotelData?.pricePerPerson}</label>
+								<div className={styles["label-subheading"]}>
+									{/* Need to handle in a better way */}
+									{hotelData?.hotel?.content?.hotelDescription.slice(0, 350)+"..."}
 								</div>
 							</div>
 							<div className={styles["col"]}>
 								<div className={styles["grid-hotel-details"]}>
 									<div className={styles["col"]}>
-										<h4><img src={hotelData?.hotel?.tripAdvisor?.ratingImageUrl} alt="ratings" /></h4>
+										<img src={hotelData?.hotel?.tripAdvisor?.ratingImageUrl} alt="ratings" />
 									</div>
 									<div className={styles["col"]}>
-										<h4 className={styles["ratings-text"]}>Rating</h4>
+										<label className={styles["ratings-text"]}>Rating</label>
 										<div className={styles["ratings-text"]}>
 											{hotelData?.hotel?.content?.starRating ?
 												hotelData?.hotel.content.starRating : 0}
 										</div>
 									</div>
 									<div className={styles["col"]}>
-										<h4 className={styles["reviews-text"]}>Reviews</h4>
+										<label className={styles["reviews-text"]}>Reviews</label>
 										<div className={styles["reviews-text"]}>
 											{hotelData?.hotel?.tripAdvisor?.numReviews ?
 												hotelData?.hotel.tripAdvisor.numReviews : 0}
@@ -109,7 +109,7 @@ const SearchResult = (props: BookingResponse) => {
 						</div>
 					))}
 					</div>) : (
-						<h4>No result found!</h4>
+						<label className={styles["error"]}>No result found!</label>
 					)}
 			</div>
 		</div>
